ea3e29045726e8d8ebd88b77633b5df14c56dbc0
Marc <139300806+venomspood@users.noreply.github.com>
2023-09-29 01:19:40 -0400

    Add files via upload
    this is day 2 where i learned the proper syntax for string interpolation

---
+++ 2_string interpolation/main.py
@@ -0,0 +1,6 @@
+age = input("What is your current age?")
+intage = int(age)
+days = (90 - intage) * 365
+weeks = (90 - intage) * 52
+months = (90 - intage) * 12
+print(f"You have {days} days, {weeks} weeks, {months} months left.")
