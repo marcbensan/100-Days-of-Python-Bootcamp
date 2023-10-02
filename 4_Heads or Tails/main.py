effc8fde9bdbe2f89f6a9c2b576e4c228a8237c6
Marc <139300806+venomspood@users.noreply.github.com>
2023-10-02 13:33:27 -0400

    Add files via upload

---
+++ 4_Heads or Tails/main.py
@@ -0,0 +1,13 @@
+import random
+
+randominteger = random.randint(1, 10)
+print(randominteger)
+
+randomfloat = random.random() * 5
+print(randomfloat)
+
+randoms = random.radint(0, 1)
+if randoms == 0:
+  print("Heads")
+else:
+  print("Tails")
